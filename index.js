caaa9b26835423271f434a88e165de4bb43639ce
Remmy4873 <Remmykiplangat4873@gmail.com>
2024-06-29 20:45:41 +0300

    add fontions file

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
 // Code your solution in this file!
+
+function distanceFromHqInBlocks(block) {
+  const blockLocation =42;
+  return Math.abs(block- blockLocation);  
+}
+
+
+function distanceFromHqInFeet(block) {
+    const blockLocation =42;
+    const blockToBlock = 264;
+
+    const blocksDistance = Math.abs(block - blockLocation);
+    const distanceInFeet = blocksDistance * blockToBlock;
+
+    return distanceInFeet;
+} 
+
+function distanceTravelledInFeet(startingPoint, endingPoint) {
+    const blockToBlock = 264; // Hypothetical value
+  
+    const blockLocation = Math.abs(endingPoint - startingPoint);
+    const distanceInFeet = blockLocation * blockToBlock;
+  
+    return distanceInFeet;
+}
+
+function calculatesFarePrice(start, destination) {
+    const totalFeetTraveled = distanceTravelledInFeet(start, destination);
+
+    if(totalFeetTraveled <= 400) {
+        return 0;
+    }else if(totalFeetTraveled > 400 && totalFeetTraveled <= 2000) {
+        return 0.02 *(totalFeetTraveled - 400);
+    }else if(totalFeetTraveled > 2000 && totalFeetTraveled < 2500) {
+        return 25;
+    }else if (totalFeetTraveled > 2500) {
+        return 'cannot travel that far';
+    }
+}
